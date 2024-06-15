402651ba241b609384f075284140182c1bf0c7d1
Pewrie Bontal <0x@bontal.net>
2024-06-16 01:06:56 +0800

    initial commit

---
+++ linked_list.hpp
@@ -0,0 +1,123 @@
+/*
+* LinkedList.hpp
+* Written by : SENG1120 Staff (c1234567)
+* Modified   : 03/08/2023
+*
+* This class represents the template implementation for a LinkedList class.
+* This file should be used in conjunction with Assignment 1 for SENG1120.
+*/ 
+
+template <typename T>
+LinkedList<T>::LinkedList() 
+{
+
+}
+
+template <typename T>
+LinkedList<T>::~LinkedList() 
+{
+
+}
+
+template <typename T>
+void LinkedList<T>::push_front(const T& data) 
+{
+
+}
+
+template <typename T>
+void LinkedList<T>::push_back(const T& data) 
+{
+
+}
+
+template <typename T>
+void LinkedList<T>::insert(const T& data) 
+{
+
+}
+
+template <typename T>
+T LinkedList<T>::pop_front()
+{
+	return T();
+}
+
+template <typename T>
+T LinkedList<T>::pop_back()
+{
+	return T();
+}
+
+template <typename T>
+T LinkedList<T>::remove()
+{
+	return T();
+}
+
+template <typename T>
+void LinkedList<T>::clear()
+{
+
+}
+
+template <typename T>
+T& LinkedList<T>::front() const
+{
+
+}
+
+template <typename T>
+T& LinkedList<T>::back() const
+{
+
+}
+
+template <typename T>
+T& LinkedList<T>::get_current() const
+{
+
+}
+
+
+template <typename T>
+void LinkedList<T>::begin() 
+{
+    
+}
+
+template <typename T>
+void LinkedList<T>::end() 
+{
+    
+}
+
+template <typename T>
+void LinkedList<T>::forward() 
+{
+
+}
+
+template <typename T>
+void LinkedList<T>::backward() 
+{
+
+}
+
+template <typename T>
+int LinkedList<T>::size() const
+{
+	return 0;
+}
+
+template <typename T>
+bool LinkedList<T>::empty() const
+{
+	return false;
+}
+
+template <typename T>
+bool LinkedList<T>::search(const T& target)
+{
+	return false;
+}
